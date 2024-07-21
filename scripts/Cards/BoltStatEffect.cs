a2bdd90476e14d1fbac950be8911e2c4a75483f7
Geert van Ieperen <ieperen3039@outlook.com>
2024-07-21 22:13:53 +0200

    initial card effect

---
+++ scripts/Cards/BoltStatEffect.cs
@@ -0,0 +1,65 @@
+using Godot;
+using System;
+
+public partial class BoltStatEffect : Node2D, IEffectSource
+{
+	[Export]
+	public bool OnlyOnPlayerFire = true;
+	[Export]
+	public float SpeedAdditive = 0;
+	[Export]
+	public float SpeedMultiplicative = 1;
+	[Export]
+	public int DamageAdditive = 0;
+	[Export]
+	public float DamageMultiplicative = 1;
+	[Export]
+	public float HomingDegPerSecond = 0;
+
+
+	public override void _Ready()
+	{
+		RichTextLabel cardText = GetNode<RichTextLabel>("CardVisual/CardText");
+		cardText.Text = GetCardText();
+	}
+
+	public void OnBoltSpawn(in Bolt aBolt, Bolt.SpawnModifiers aMod, bool aPlayerFire)
+	{
+		if (OnlyOnPlayerFire && !aPlayerFire) return;
+
+		aMod.SpeedAdditive += SpeedAdditive;
+		aMod.SpeedMultiplicative *= SpeedMultiplicative;
+		aMod.DamageAdditive += DamageAdditive;
+		aMod.DamageMultiplicative *= DamageMultiplicative;
+		aMod.HomingDegPerSecond += HomingDegPerSecond;
+	}
+
+	public void OnEnemyBoltCollision(
+		in Bolt aBolt, in Enemy aEnemy, Bolt.CollisionModifiers aBoltMod, Enemy.CollisionModifiers aEnemyMod, Level.CollisionModifiers aLevelMod
+	)
+	{ }
+
+	public string GetCardText()
+	{
+		string text = OnlyOnPlayerFire ? "Player bolts" : "All bolts";
+		if (SpeedAdditive != 0) text += string.Format("\n+{0} projectile speed", SpeedAdditive);
+		if (SpeedMultiplicative != 1) text += string.Format("\n{0}x projectile speed", SpeedMultiplicative);
+		if (DamageAdditive != 0) text += string.Format("\n+{0} projectile damage", DamageAdditive);
+		if (DamageMultiplicative != 1) text += string.Format("\n{0}x projectile damage", DamageMultiplicative);
+
+		if (HomingDegPerSecond > 90)
+		{
+			text += "\nmajor homing";
+		}
+		else if (HomingDegPerSecond > 45)
+		{
+			text += "\nmedium homing";
+		}
+		else if (HomingDegPerSecond > 0)
+		{
+			text += "\nminor homing";
+		}
+
+		return text;
+	}
+}
